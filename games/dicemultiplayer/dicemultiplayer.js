a81861d249901e349f37dfb11dbef5c6f47591e9
JimmyN69 <lamcaokiet@gmail.com>
2026-09-16 20:56:28 -0400

    made dice multiplayer ui

---
+++ games/dicemultiplayer/dicemultiplayer.js
@@ -0,0 +1,9 @@
+const username = document.getElementById("username");
+
+const createLobbyButton = document.getElementById("createLobbyButton");
+
+const lobbyId = document.getElementById("lobbyId");
+
+const joinLobbyButton = document.getElementById("joinLobbyButton");
+
+const lobbyStatus = document.getElementById("lobbyStatus");
